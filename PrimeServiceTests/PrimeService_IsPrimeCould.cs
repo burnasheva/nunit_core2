c338119cabda6581b599d2d5e0a8cc863408c3c5
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-07-11 14:40:50 +0200

    add output to log

--- PrimeServiceTests/PrimeService_IsPrimeCould.cs
+++ PrimeServiceTests/PrimeService_IsPrimeCould.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Prime.Services;
 
@@ -16,6 +17,10 @@ namespace Prime.UnitTests.Services
         [Test]
         public void ReturnFalseGivenValueOf2()
         {
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
+            TestContext.WriteLine("message from TestContext");
+            
             var result = _primeService.IsPrime(2);
 
             Assert.IsFalse(result, $"2 should not be prime");
@@ -25,6 +30,10 @@ namespace Prime.UnitTests.Services
         [Ignore("temporary ignored test")]
         public void IgnoredTest()
         {
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
+            TestContext.WriteLine("message from TestContext");
+            
             var result = _primeService.IsPrime(2);
 
             Assert.IsFalse(result, $"2 should not be prime");
