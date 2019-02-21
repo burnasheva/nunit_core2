12b2d407a786de3f20c74ce5a0563455e48cd2c8
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2019-02-21 08:51:27 +0100

    update version and dotnet core

--- PrimeServiceTests/PrimeService_IsPrimeCould2.cs
+++ PrimeServiceTests/PrimeService_IsPrimeCould2.cs
@@ -29,13 +29,13 @@ namespace Prime.UnitTests.Services
         [Test]
         public void ThisTestShouldFail()
         {
-            Console.WriteLine("some stdOut text");
+            Console.WriteLine("some stdOut new text");
             Console.Error.WriteLine("some stdErr text");
             TestContext.WriteLine("message from TestContext");
             
             var result = _primeService.IsPrime(10);
 
-            Assert.IsFalse(result, $"10 should not be prime");
+            Assert.IsTrue(result, $"10 should not be prime");
         }
     }
 }
